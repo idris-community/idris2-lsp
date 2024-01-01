57448d5900df496b73c7e597888b9240abed3be8
Mathew Polzin <matt.polzin@gmail.com>
2023-12-31 22:17:27 -0600

    apply formatter

--- flake.nix
+++ flake.nix
@@ -23,42 +23,46 @@
     };
   };
 
-  outputs = { self, nixpkgs, alejandra, idris, lsp-lib }:
-    let
-      lib = nixpkgs.lib;
-      # support the same systems as Idris2
-      systems = builtins.attrNames idris.packages;
-      forEachSystem = with lib; mkOutputs: 
-        let outputsForSystem = system: 
-          concatMapAttrs (k: v: { ${k}.${system} = v; }) (mkOutputs system);
-        in
-        foldl' recursiveUpdate { } (map outputsForSystem systems);
+  outputs = {
+    self,
+    nixpkgs,
+    alejandra,
+    idris,
+    lsp-lib,
+  }: let
+    lib = nixpkgs.lib;
+    # support the same systems as Idris2
+    systems = builtins.attrNames idris.packages;
+    forEachSystem = with lib; mkOutputs: let
+      outputsForSystem = system:
+        concatMapAttrs (k: v: {${k}.${system} = v;}) (mkOutputs system);
     in
-    forEachSystem (system:
-      let
+      foldl' recursiveUpdate {} (map outputsForSystem systems);
+  in
+    forEachSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
         idrisPkgs = idris.packages.${system};
         buildIdris = idris.buildIdris.${system};
         lspLibPkg = lsp-lib.packages.${system};
-        supportLibrariesPath = lib.makeLibraryPath [ idrisPkgs.support ];
-        supportSharePath = lib.makeSearchPath "share" [ idrisPkgs.support ];
+        supportLibrariesPath = lib.makeLibraryPath [idrisPkgs.support];
+        supportSharePath = lib.makeSearchPath "share" [idrisPkgs.support];
 
-        globalLibraries =
-          let 
-            idrName = "idris2-${idris.version}";
-            libSuffix = "lib/${idrName}";
-          in [
-            "\\$HOME/.nix-profile/lib/${idrName}"
-            "/run/current-system/sw/lib/${idrName}"
-            "${idrisPkgs.idris2}/${idrName}"
-          ];
+        globalLibraries = let
+          idrName = "idris2-${idris.version}";
+          libSuffix = "lib/${idrName}";
+        in [
+          "\\$HOME/.nix-profile/lib/${idrName}"
+          "/run/current-system/sw/lib/${idrName}"
+          "${idrisPkgs.idris2}/${idrName}"
+        ];
         globalLibrariesPath = builtins.concatStringsSep ":" globalLibraries;
 
         lspPkg = buildIdris {
           projectName = "idris2-lsp";
           src = ./.;
-          idrisLibraries = [ idrisPkgs.idris2-api lspLibPkg.lsp-lib ];
-          buildInputs = [ pkgs.makeWrapper ];
+          idrisLibraries = [idrisPkgs.idris2-api lspLibPkg.lsp-lib];
+          buildInputs = [pkgs.makeWrapper];
           postInstall = ''
             wrapProgram $out/bin/idris2-lsp \
               --run 'export IDRIS2_PREFIX=''${IDRIS2_PREFIX-"$HOME/.idris2"}' \
@@ -70,11 +74,13 @@
           '';
         };
       in rec {
-      packages = rec {
-        idris2-lsp = lspPkg.executable; 
-        default = idris2-lsp;
-      } // idrisPkgs;
-      formatter = alejandra.packages.${system}.default;
-    }
-  );
+        packages =
+          rec {
+            idris2-lsp = lspPkg.executable;
+            default = idris2-lsp;
+          }
+          // idrisPkgs;
+        formatter = alejandra.packages.${system}.default;
+      }
+    );
 }
